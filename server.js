bdf2691d6787a3c3646dbe0a65405a3970c3abcb
MayHagbi <mayhagbi@gmail.com>
2020-10-20 16:56:37 +0300

    modified server file for working with router file

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express")
+const router = require('./router')
 const app = express()
 
 app.use(express.static('public'))
-// let express know where to find our views
-app.set('views', 'views')
+app.set('views', 'views') // let express know where to find our views
 app.set('view engine', 'ejs')
-
-app.get('/', function(req, res) {
-    res.render('home_page')
-})
+app.use('/', router)
 
 app.listen(5000)
