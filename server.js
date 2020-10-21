7209871a60d45d32f20efe6ed03177f5c6bc4ee7
MayHagbi <mayhagbi@gmail.com>
2020-10-21 13:23:46 +0300

    update server file

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express")
 const router = require('./router')
 const app = express()
 
+app.use(express.urlencoded({extended: false})) // tell express to submiting the user data in the url
+app.use(express.json())
+
 app.use(express.static('public'))
 app.set('views', 'views') // let express know where to find our views
 app.set('view engine', 'ejs')
